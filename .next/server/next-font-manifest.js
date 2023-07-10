53d6ae870a907d5fde87a3d0e22a294b4c928d2d
Tiuko <108450912+Tiuko@users.noreply.github.com>
2023-07-10 15:36:42 +0200

    Database &  SupabaseProvider created

--- .next/server/next-font-manifest.js
+++ .next/server/next-font-manifest.js
@@ -1,1 +1,1 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/peter/Coding/Spotify-clone/app/layout\":[\"static/media/afeb3c3e21dfbc9a-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{},\"appUsingSizeAdjust\":false,\"pagesUsingSizeAdjust\":false}"
